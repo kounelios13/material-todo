2ec9ce6b9a58bf70ce27a1691106567b2986af90
Manos Kounelakis <pkounelios@gmail.com>
2019-07-14 16:26:20 +0300

    aria label changed

--- src/components/TodoGrid.jsx
+++ src/components/TodoGrid.jsx
@@ -9,6 +9,7 @@ import IconButton from "@material-ui/core/IconButton";
 import ListItemText from "@material-ui/core/ListItemText";
 // import CommentIcon from "@material-ui/icons/Comment";
 import DeleteIcon from "@material-ui/icons/Delete";
+
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 const useStyles = makeStyles(theme => ({
@@ -53,10 +54,11 @@ function TodoGrid(props) {
               />
             </ListItemIcon>
             <ListItemText id={`label-id-${todo.id}`} primary={todo.title} />
+
             <ListItemSecondaryAction>
               <IconButton
                 edge="end"
-                aria-label="Comments"
+                aria-label="Delete todo"
                 onClick={() => props.deleteTodo(todo)}
               >
                 <DeleteIcon />
